831072ba734a9f8f0bf1fcb348679f1118fbd950
danggkhoa2209 <pndgkhoa2000@gmail.com>
2022-05-15 17:52:10 +0700

    Manage user's post page/fix

--- DemoDB2/DemoDB2/Models/DBRaoVatModel.Designer.cs
+++ DemoDB2/DemoDB2/Models/DBRaoVatModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\RAOVAT\DemoDB2\DemoDB2\Models\DBRaoVatModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Code\Đồ án\CNPMNC\Đồ Án\RaoVatProject\DemoDB2\DemoDB2\Models\DBRaoVatModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
---
+++ RaoVat/RaoVat/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RaoVat.Models;
+
+namespace RaoVat.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        DBRaoVatEntities db = new DBRaoVatEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(db.RAOVATs.Include("User").OrderByDescending(x => x.NGAYGIODANG).Where(s => s.MATRANGTHAI == 1).Take(6).ToList());
+        }
+        public ActionResult TrangChu()
+        {
+            return View(db.RAOVATs.Include("User").OrderByDescending(x => x.NGAYGIODANG).Where(s => s.MATRANGTHAI == 1).Take(6).ToList());
+
+        }
+    }
+}
